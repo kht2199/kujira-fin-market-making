2ac3f2677f41b9fc69d91fc4d42b70aaf7aafdc8
heetaek.kim <develop@kakao.com>
2022-07-26 00:17:56 +0900

    chore: message value, balance

--- src/app/trading-state-executor.ts
+++ src/app/trading-state-executor.ts
@@ -59,7 +59,7 @@ export class TradingStateExecutor {
         const { baseAmount, quoteAmount} = trading.balance;
         const value = trading.balance.calculateValue(marketPrice).toFixed(5);
         balanceRate = trading.balance.calculateRate(marketPrice);
-        message = `[stat] value: ${value} ${quoteSymbol}, balance rate: ${balanceRate.toFixed(5)}, target rate: ${targetRate.toFixed(5)}, base: ${baseAmount.toFixed(5)} ${baseSymbol}, quote: ${quoteAmount.toFixed(5)} ${quoteSymbol}`;
+        message = `[stat] value: ${(baseAmount * quoteAmount).toFixed(5)} balance: ${value} ${quoteSymbol}, balance rate: ${balanceRate.toFixed(5)}, target rate: ${targetRate.toFixed(5)}, base: ${baseAmount.toFixed(5)} ${baseSymbol}, quote: ${quoteAmount.toFixed(5)} ${quoteSymbol}`;
         kujira.sendMessage(message);
         TradingStateExecutor.logger.debug(message)
         let tps: OrderMarketMaking[] = deltaRates
