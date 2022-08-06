ef23a6c435dae707c6e1853c4971486e4e7a2852
heetaek.kim <develop@kakao.com>
2022-08-06 23:43:30 +0900

    fix: balance getter

--- src/app/trading-balance.ts
+++ src/app/trading-balance.ts
@@ -1,25 +1,33 @@
 
 export class TradingBalance {
 
-  public readonly baseAmount: number;
+  private readonly _baseAmount: number;
 
-  public readonly quoteAmount: number;
+  private readonly _quoteAmount: number;
 
   constructor(
     public base: Balance, public quote: Balance
   ) {
-    this.baseAmount = Number(this.base.amount);
-    this.quoteAmount = Number(this.quote.amount);
+    this._baseAmount = Number(this.base.amount);
+    this._quoteAmount = Number(this.quote.amount);
   }
 
   public calculateRate(marketPrice: number): number {
-    const baseValue = this.baseAmount * marketPrice;
-    const totalValue = baseValue + this.quoteAmount;
+    const baseValue = this._baseAmount * marketPrice;
+    const totalValue = baseValue + this._quoteAmount;
     return baseValue / totalValue;
   }
 
   public calculateValue(marketPrice: number): number {
-    const baseValue = this.baseAmount * marketPrice;
-    return baseValue + this.quoteAmount;
+    const baseValue = this._baseAmount * marketPrice;
+    return baseValue + this._quoteAmount;
+  }
+
+  get baseAmount(): number {
+    return this._baseAmount;
+  }
+
+  get quoteAmount(): number {
+    return this._quoteAmount;
   }
 }
