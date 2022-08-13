8692e34df25fed2be8e3622383ed3309be35fed6
heetaek.kim <develop@kakao.com>
2022-08-13 13:34:16 +0900

    feat: add swagger-ui

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from "./http-exception.filters";
 import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
+
+  setupSwagger(app);
+
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
@@ -20,5 +24,17 @@ async function bootstrap() {
     })
   );
   await app.listen(3000, '0.0.0.0');
+}
+
+function setupSwagger(app: NestFastifyApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('NestJS API Docs')
+    .setDescription('NestJS API description')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
 }
 (async () => await bootstrap())();
