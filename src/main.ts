73b3bc39fa636cb662f3da635f65185d27de7df4
heetaek.kim <develop@kakao.com>
2022-08-13 12:46:30 +0900

    fix: express to fastify

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory, Reflector } from "@nestjs/core";
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from "./http-exception.filters";
 import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
